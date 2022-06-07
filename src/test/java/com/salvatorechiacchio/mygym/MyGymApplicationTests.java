44aa3195f3707577b614df94fb7d91dc8e38dfdd
villaflaminio <vifla01@gmail.com>
2022-06-07 10:24:43 +0200

    add security

---
+++ src/test/java/com/salvatorechiacchio/mygym/MyGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvatorechiacchio.mygym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
