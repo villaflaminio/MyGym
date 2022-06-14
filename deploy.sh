62012fc0db90f939cffe7073e91465df610eaf86
villaflaminio <vifla01@gmail.com>
2022-06-14 16:11:57 +0200

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+git pull
+mvn clean install
+docker stop mygym
+docker rm mygym
+docker build -t mygym .
+docker run -d --name mygym -p 4004:4004 --add-host host.docker.internal:172.17.0.1 mygym --restart always
