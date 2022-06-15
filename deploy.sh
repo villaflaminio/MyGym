bb25c2f88df0b69bee190c475f118ef1f6f25d7d
villaflaminio <vifla01@gmail.com>
2022-06-15 09:38:24 +0200

    Add --restart=always to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ mvn clean install
 docker stop mygym
 docker rm mygym
 docker build -t mygym .
-docker run -d --name mygym -p 4004:4004 --add-host host.docker.internal:172.17.0.1 mygym --restart=always
+docker run --restart=always -d --name mygym -p 4004:4004 --add-host host.docker.internal:172.17.0.1 mygym
